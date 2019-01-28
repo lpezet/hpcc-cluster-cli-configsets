8bcd424dd473ce1ad167809bbcb6e4670a68bf19
Luke Pezet <lpezet@gmail.com>
2019-01-27 22:58:08 -0700

    Now supporting Ubuntu install as well.

--- scripts/install_hpcc.sh
+++ scripts/install_hpcc.sh
@@ -1,13 +1,79 @@
 #!/bin/bash
 
+os=
+
+figure_out_extension() {
+  os=$(hostnamectl | grep "Operating System" | cut -c21-)
+  case $os in
+    "CentOS Linux 7"*)
+      extension=".el7.x86_64.rpm"
+      os=centos
+      ;;
+    "Amazon Linux 2")
+      extension=".el7.x86_64.rpm"
+      os=centos
+      ;;
+    "CentOS Linux 6"*)
+      extension=".el6.x86_64.rpm"
+      os=centos
+      ;;
+    "Amazon Linux")
+      extension=".el6.x86_64.rpm"
+      os=centos
+      ;;
+    "Ubuntu 14."*)
+      extension="trusty_amd64.deb"
+      os=ubuntu
+      ;;
+    "Ubuntu 16."*)
+      extension="xenial_amd64.deb"
+      os=ubuntu
+      ;;
+    "Ubuntu 18."*)
+      extension="bionic_amd64.deb"
+      os=ubuntu
+      ;;
+    *)
+      extension=".el6.x86_64.rpm"
+      os=centos
+      ;;
+  esac
+}
+
+install_hpcc() {
+  echo "Installing HPCC Systems ${HPCC_VERSION}..." >> /var/log/hpcc-cluster.log
+  case $os in
+    centos)
+      yum -y install /tmp/hpccsystems-platform-community_${build_version}${extension}
+      ;;
+    ubuntu)
+      dpkg -i /tmp/hpccsystems-platform-community_${build_version}${extension}
+      apt-get install -f
+      ;;
+   esac
+}
+
+install_tools() {
+  echo "Installing tools..." >> /var/log/hpcc-cluster.log
+  case $os in
+    centos)
+      yum -y install httpd-tools
+      ;;
+    ubuntu)
+      apt-get install apache2-utils
+      ;;
+   esac
+}
+
 build_version=${HPCC_VERSION}
 version=$(echo ${HPCC_VERSION} | sed "s/-.*//g")
-echo "Downloading HPCC Systems ${HPCC_VERSION}..." >> /var/log/hpcc-cluster.log
-curl -o /tmp/hpccsystems-platform-community_${build_version}.el6.x86_64.rpm http://cdn.hpccsystems.com/releases/CE-Candidate-${version}/bin/platform/hpccsystems-platform-community_${build_version}.el6.x86_64.rpm
-echo "Installing HPCC Systems ${HPCC_VERSION}..." >> /var/log/hpcc-cluster.log
-yum -y install /tmp/hpccsystems-platform-community_${build_version}.el6.x86_64.rpm
-echo "Installing httpd tools..." >> /var/log/hpcc-cluster.log
-yum -y install httpd-tools
+figure_out_extension
+
+echo "Downloading HPCC Systems ${HPCC_VERSION}${extension}..." >> /var/log/hpcc-cluster.log
+curl -o /tmp/hpccsystems-platform-community_${build_version}${extension} http://cdn.hpccsystems.com/releases/CE-Candidate-${version}/bin/platform/hpccsystems-platform-community_${build_version}${extension}
+
+install_hpcc
+install_tools
 
 
 # ##########################################
